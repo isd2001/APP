6b07ae36474b9c40cf720757695ec5ee6d8d0158
MR <tetsu2721@gmail.com>
2021-08-03 16:25:38 +0900

    학습 콘텐츠 조회 수정

--- src/main/java/codegurus/learning/vo/ContentsVO.java
+++ src/main/java/codegurus/learning/vo/ContentsVO.java
@@ -4,22 +4,21 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 /**
- * 학습 VO
+ * 콘텐츠 VO
  */
 @Getter
 @Setter
-public class LearningVO {
+public class ContentsVO {
 
     @ApiModelProperty(notes = "온라인 과목 스케줄 ID", example="2", position = 1)
     private String onlineSubjectScheduleId;
 
-    @ApiModelProperty(notes = "콘텐츠 ID", example="2", position = 2)
+    @ApiModelProperty(notes = "콘텐츠 ID", example = "1")
     private String contentsId;
 
-    @ApiModelProperty(notes = "책 ID", example = "139", position = 3)
-    private String bookId;
-
-    @ApiModelProperty(notes = "책 정보")
-    private BookVO book;
+    @ApiModelProperty(notes = "템플릿 인스턴스 리스트")
+    private List<TemplateInstVO> templateInstList;
 }
