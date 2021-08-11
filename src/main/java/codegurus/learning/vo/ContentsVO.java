cff3f31d3d2cd561878ab8c16c321e29d521b82e
MR <tetsu2721@gmail.com>
2021-08-11 09:40:58 +0900

    학습 콘텐츠 조회 수정

--- src/main/java/codegurus/learning/vo/ContentsVO.java
+++ src/main/java/codegurus/learning/vo/ContentsVO.java
@@ -19,6 +19,12 @@ public class ContentsVO {
     @ApiModelProperty(notes = "콘텐츠 ID", example = "1")
     private String contentsId;
 
+    @ApiModelProperty(notes = "콘텐츠 명", example = "테스트 콘텐츠")
+    private String contentsTitle;
+
     @ApiModelProperty(notes = "템플릿 인스턴스 리스트")
     private List<TemplateInstVO> templateInstList;
+
+    @ApiModelProperty(notes = "책 정보")
+    private BookVO book;
 }
